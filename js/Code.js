39624e944f5f216e3f5744c09fa2eab00b51d02f
EvelioVallarta24 <91225966+EvelioVallarta24@users.noreply.github.com>
2022-08-09 13:49:49 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "CERDO";
+const accessCode2 = "CABALLO";
+const accessCode3 = "PERRO";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E D R O C", 100,50)
+    fill("lightblue")
+    text("Pista: Soy rosita y muy sucio", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("L L O A B C A", 700,150)
+    fill("lightblue")
+    text("Pista: Tengo silla pero no puedo sentarme", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("R R E O P", 100,250)
+    fill("lightblue")
+    text("Pista: Guau de día, guau de noche, cazo y ladro", 100,270)
+}
